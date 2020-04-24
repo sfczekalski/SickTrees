999aa8d29ab3c8e2551b68b1d145ca8324925692
sfczekalski <sfczekalski@gmail.com>
2020-04-24 20:19:07 +0200

    Function to save image in storage.

--- app/src/main/java/com/example/sickTrees/Result.kt
+++ app/src/main/java/com/example/sickTrees/Result.kt
@@ -8,14 +8,22 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageMetadata
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.UploadTask
 import kotlinx.android.synthetic.main.activity_result.*
 import java.io.ByteArrayOutputStream
+import java.util.*
 
 
 class Result : AppCompatActivity() {
 
     lateinit var PhotoPath: String
     lateinit var imageBitmap: Bitmap
+    lateinit var pred: String
+
+    val storage: FirebaseStorage = FirebaseStorage.getInstance()
 
         override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,7 +40,7 @@ class Result : AppCompatActivity() {
         imageView.setImageBitmap(imageBitmap)
 
         // Get prediction from intent
-        val pred = intent.getStringExtra("pred")
+        pred = intent.getStringExtra("pred")
         Log.i("SickTrees", pred)
 
         // Set prediction in layout
@@ -52,6 +60,14 @@ class Result : AppCompatActivity() {
         val stream = ByteArrayOutputStream()
         imageBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
         val byteArray = stream.toByteArray()
+
+        val path: String = "images/" + UUID.randomUUID() + ".png"
+        val firebaseRef: StorageReference = storage.getReference(path)
+
+        val meta: StorageMetadata = StorageMetadata.Builder().
+            setCustomMetadata("label", pred).
+            build()
+        val uploadTask: UploadTask = firebaseRef.putBytes(byteArray, meta)
     }
 
 }
